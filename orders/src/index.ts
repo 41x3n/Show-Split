0697e8f3e03569e4d896229758402f0311e02d9e
Anindya Chowdhury <anindya50@live.com>
2021-06-16 00:35:32 +0530

    update start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,7 +9,7 @@ import { PaymentCreatedListener } from "./events/listeners/payment-created-liste
 import { DatabaseConnectionError } from "@showsplit/common";
 
 const start = async () => {
-  console.log("Starting...");
+  console.log("Starting....");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
